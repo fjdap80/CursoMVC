fd608d3aa23eed06c322eca6f14b51487011b16e
Fco Javier <fjdap80@gmail.com>
2023-10-30 22:04:31 +0100

    30/10/23 2a Parte

--- CapaPresentacionAdmin/Controllers/AccesoController.cs
+++ CapaPresentacionAdmin/Controllers/AccesoController.cs
@@ -40,22 +40,47 @@ namespace CapaPresentacionAdmin.Controllers
                     TempData["IdUsuario"] = oUsuario.IdUsuario;
                     return RedirectToAction("CambiarClave");
                 }
-            
+
                 ViewBag.Error = null;
                 return RedirectToAction("Index", "Home");
             }
-           
+
         }
         [HttpPost]
-        public ActionResult CambiarClave(string idusuario,string claveactual,string nuevaclave,string confirmarclave)
+        public ActionResult CambiarClave(string idusuario, string claveactual, string nuevaclave, string confirmarclave)
         {
             Usuario oUsuario = new Usuario();
-            oUsuario= new CN_Usuarios().Listar().Where(u => u.IdUsuario==int.Parse(idusuario)).FirstOrDefault();
-            if ()
+            oUsuario = new CN_Usuarios().Listar().Where(u => u.IdUsuario == int.Parse(idusuario)).FirstOrDefault();
+            if (oUsuario.Clave != CN_Recursos.ConvertirSha256(claveactual))
             {
-
+                TempData["IdUsuario"] = idusuario;
+                ViewData["vclave"] = "";
+                ViewBag.Error = "La contraseña actual no es correcta";
+                return View();
             }
-            return View();
+            else if (nuevaclave != confirmarclave)
+            {
+                TempData["IdUsuario"] = idusuario;
+                ViewData["vclave"] = claveactual;
+                ViewBag.Error = "Las contraseñas no coinciden";
+                return View();
+            }
+            ViewData["vclave"] = "";
+
+            nuevaclave = CN_Recursos.ConvertirSha256(nuevaclave);
+
+            string mensaje = string.Empty;
+            bool respuesta = new CN_Usuarios().CambiarClave(int.Parse(idusuario), nuevaclave, out mensaje);
+            if (respuesta)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["IdUsuario"] = idusuario;
+                ViewBag.Error = mensaje;
+                return View();
+            }
         }
     }
 }
